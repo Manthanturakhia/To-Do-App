89bc07588397b3f5fbb1056d601756e4de53ba0f
Manthan <manthansturakhia@gmail.com>
2020-10-06 12:47:05 +0530

    creating model

--- todoapp/my_app/views.py
+++ todoapp/my_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from my_app.models import ToDo
+from . import models
+
 
 # Create your views here.
+def home(request):
+    return render(request, template_name = 'my_app/index.html')
+
+def add_todo(request):
+    added_date = timezone.now()
+    content = request.POST["form_content"]
+    models.ToDo.objects.create(added_date=added_date, text=content)
+    return render(request, template_name = 'my_app/index.html')
